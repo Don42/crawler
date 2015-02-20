30e804ba260f2a40211ca6fdacbe635e7ce8c7aa
Marco don Kaulea <DonMarco42@gmail.com>
2015-02-20 21:54:20 +0100

    Recursively download categories

--- jamieoliver.py
+++ jamieoliver.py
@@ -11,15 +11,20 @@
 
 Usage:
     jamieoliver recipes <url>
+    jamieoliver cat <url>
     jamieoliver categories
 '''
 
 import bs4
 import docopt
+import functools
 import requests
 
+BASE_URL = 'http://www.jamieoliver.com'
 
-def download_recipe(url):
+
+def download_recipe(url, base=""):
+    url = base + url
     r = requests.get(url)
     r.encoding = 'utf-8'
     return r.text
@@ -36,22 +41,49 @@ def parse_recipe(page):
             'ingredients': ingredients_list}
 
 
-def download_categories():
-    url = 'http://www.jamieoliver.com/'
+def download_main_categories(url):
     r = requests.get(url)
     r.encoding = 'utf-8'
     soup = bs4.BeautifulSoup(r.text)
     categories_div = soup.find('div', {'class': 'cat_secondary'})
     categories_a = categories_div.find_all('a')
     link_list = [x['href'] for x in categories_a]
-    non_recipe_cats = ['http://www.jamieoliver.com/recipes/category/occasion',
-                       'http://www.jamieoliver.com/recipes/category/books']
-    for x in non_recipe_cats:
-        try:
-            link_list.remove(x)
-        except ValueError:
-            pass
     return link_list
+
+
+def download_categories(url):
+    categories = download_main_categories(url)
+    for x in categories:
+        page = download_recipe(x)
+        subcategories = detect_subcategories(page)
+        if subcategories is not None:
+            subcategories = parse_subcategories(subcategories)
+            subcategories = map(
+                functools.partial(prepend_base_url, BASE_URL),
+                subcategories)
+            categories.extend(subcategories)
+    return set(categories)
+
+
+def detect_subcategories(page):
+    soup = bs4.BeautifulSoup(page)
+    section = soup.find('section', {'id': 'category_top'})
+    if section is not None:
+        section = soup.find('section', {'id': 'recipe_collections'})
+    return section
+
+
+def parse_subcategories(section):
+    categories_articles = section.find_all('article')
+    link_list = [x.a['href'] for x in categories_articles]
+    return link_list
+
+
+def prepend_base_url(base, url):
+    if 'http' in url:
+        return url
+    else:
+        return base + url
 
 
 def main():
@@ -60,8 +92,12 @@ def main():
         page = download_recipe(arguments['<url>'])
         recipe = parse_recipe(page)
         print(recipe)
+    elif arguments.get('cat', False):
+        page = download_recipe(arguments['<url>'])
+        print(parse_subcategories(detect_subcategories(page)))
     else:
-        for x in download_categories():
+        categories = download_categories(BASE_URL)
+        for x in categories:
             print(x)
 
 
