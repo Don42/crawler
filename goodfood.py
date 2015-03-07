102fdeb8c10d58b5303bf7e682a0e12ac857508c
Marco don Kaulea <DonMarco42@gmail.com>
2015-03-07 12:46:52 +0100

    Add category crawler

--- goodfood.py
+++ goodfood.py
@@ -9,12 +9,18 @@
 """goodfood
 
 Usage:
-    goodfood <url>
+    goodfood recipe <url>
+    goodfood categories <url>
 """
 
 import bs4
 import docopt
+import re
 import requests
+
+
+def make_absolute(url):
+    return 'http://www.bbcgoodfood.com{}'.format(url)
 
 
 def parse_meta_tag(page, itemprop):
@@ -50,11 +56,41 @@ def parse_recipe(page):
     return out
 
 
+def get_subcategories(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        raise Exception("Request error")
+    soup = bs4.BeautifulSoup(r.text)
+    main_content = soup.find('div', {'id': 'main-content'})
+    links = [x['href'] for x in main_content.findAll('a')]
+    regex = re.compile('^/recipes/collection/[^/]*$')
+    links = filter(regex.match, links)
+    return links
+
+
+def get_categories(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        raise Exception("Request error")
+    soup = bs4.BeautifulSoup(r.text)
+    navigation = soup.find('div', {'id': 'nav-touch', 'class': 'nav-touch'})
+    links = [x['href'] for x in navigation.findAll('a')]
+    regex = re.compile('^/recipes/category/[^/]*$')
+    links = filter(regex.match, links)
+    for category in links:
+        yield get_subcategories(make_absolute(category))
+
+
 def main():
     arguments = docopt.docopt(__doc__)
-    r = requests.get(arguments['<url>'])
-    if r.status_code == 200:
-        print(parse_recipe(r.text))
+    if arguments.get('recipe', False):
+        r = requests.get(arguments['<url>'])
+        if r.status_code == 200:
+            print(parse_recipe(r.text))
+    elif arguments.get('categories', False):
+        for x in get_categories(arguments['<url>']):
+            for y in x:
+                print(y)
 
 
 if __name__ == '__main__':
