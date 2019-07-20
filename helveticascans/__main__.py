b87b9ac2f5113bb51f7f1a4e7581de89e668e55f
Marco 'don' Kaulea <don@0xbeef.org>
2019-07-20 16:58:49 +0200

    Implement rudimentary argument parsing
    The code is very unclean, but works for now.

--- helveticascans/__main__.py
+++ helveticascans/__main__.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import pathlib as pl
 
@@ -8,13 +9,29 @@ from helveticascans.volume import group_volumes
 kyuu_chan_base_url = "https://helveticascans.com/r/series/wonder-cat-kyuu-chan/"
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Download volumes")
+    parser.add_argument("volume_number", type=int, nargs='?')
+    args = parser.parse_args()
+    return args
+
+
 async def main():
+    args = parse_arguments()
+    download_path = pl.Path("./download")
     pages_list = get_page_list(kyuu_chan_base_url)
     pages_list = await uris_to_pages(pages_list)
     volumes = group_volumes(pages_list.__iter__())
-    for i, x in enumerate(volumes):
-        await x.write_to_file(pl.Path("./download") / f"kyuu_{i+1}.cbz")
-    print(volumes)
+    if args.volume_number is not None:
+        i = args.volume_number
+        await volumes[i - 1].write_to_file(
+            download_path / f"kyuu_{i}.cbz"
+        )
+    else:
+        for i, x in enumerate(volumes):
+            await x.write_to_file(
+                download_path / f"kyuu_{i + 1}.cbz"
+            )
 
 
 if __name__ == '__main__':
