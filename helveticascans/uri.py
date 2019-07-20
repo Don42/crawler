3b900f2e5ec33be2853a573b6f0109932ea8e844
Marco 'don' Kaulea <don@0xbeef.org>
2019-07-20 16:29:03 +0200

    Add some error handling

--- helveticascans/uri.py
+++ helveticascans/uri.py
@@ -1,6 +1,7 @@
 import re
 
 import aiohttp
+from aiohttp.client_exceptions import ClientConnectionError
 from aiohttp.typedefs import StrOrURL
 from bs4 import BeautifulSoup, element
 
@@ -19,8 +20,16 @@ def parse_page_links(links: list):
     return [(item['href'], parse_page_link(item['href']).groups()) for item in links]
 
 
+async def try_get(uri, retry_count=3):
+    for _ in range(retry_count):
+        try:
+            return await session.get(uri)
+        except ClientConnectionError:
+            pass
+
+
 async def get_image_uri(page_uri):
-    response = await session.get(page_uri)
+    response = await try_get(page_uri)
     bs = BeautifulSoup(await response.text(), 'html.parser')
     image: element.Tag = (
         bs
@@ -31,13 +40,13 @@ async def get_image_uri(page_uri):
 
 
 async def get_image(image_uri: StrOrURL) -> bytes:
-    response = await session.get(image_uri)
+    response = await try_get(image_uri)
     response.raise_for_status()
     return await response.read()
 
 
 async def get_page_list(series_url: StrOrURL):
-    response = await session.get(series_url)
+    response = await try_get(series_url)
     page = BeautifulSoup(await response.text(), HTML_PARSER)
     comics = [
         div.find('a')
